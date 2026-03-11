4e4b6d88e840385a107f7aba627474ccbd8e9477
weynard02 <samsicoweynard@gmail.com>
2026-03-11 09:34:04 +0700

    feat: login register

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import LoginPage from "../page/login/LoginPage";
 import DashboardPage from "../page/dashboard/DashboardPage";
 import MakeAppointmentPage from "../page/make-appointment/MakeAppointmentPage";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/my-booking" element={<MakeAppointmentPage />} />
     </Routes>
