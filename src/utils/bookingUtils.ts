ce5019512d64a6a5c571058d2e8845cff3907aab
weynard02 <samsicoweynard@gmail.com>
2026-03-12 09:11:04 +0700

    fix: data type booking

--- src/utils/bookingUtils.ts
+++ src/utils/bookingUtils.ts
@@ -1,41 +1,39 @@
-import {
-    rooms,
-    bookings,
-    bookingSlots,
-    customers
-} from "../data/mockData";
+import { rooms, bookings, bookingSlots, customers } from "../data/mockData";
+import type MergedBooking from "../types/MergedBooking";
 
 export function getMergedBookings(date: string) {
-    const result: any[] = [];
+  const result: MergedBooking[] = [];
 
-    const bookingsToday = bookings.filter(
-        (booking) => booking.date === date
+  const bookingsToday = bookings.filter((booking) => booking.date === date);
+
+  bookingsToday.forEach((booking) => {
+    const room = rooms.find((r) => r.room_id === booking.room_id);
+    const customer = customers.find(
+      (c) => c.customer_id === booking.customer_id
     );
 
-    bookingsToday.forEach((booking) => {
-        const room = rooms.find(r => r.room_id === booking.room_id);
-        const customer = customers.find(c => c.customer_id === booking.customer_id);
+    const slots = bookingSlots
+      .filter((slot) => slot.booking_id === booking.booking_id)
+      .sort(
+        (a, b) =>
+          new Date(a.start_hour).getTime() - new Date(b.end_hour).getTime()
+      );
 
-        const slots = bookingSlots
-            .filter(slot => slot.booking_id === booking.booking_id)
-            .sort((a, b) => new Date(a.start_hour).getTime() - new Date(b.end_hour).getTime());
+    if (slots.length === 0) return;
 
-        if (slots.length === 0) return;
+    const start = new Date(slots[0].start_hour);
+    const end = new Date(slots[slots.length - 1].end_hour);
 
-        const start = new Date(slots[0].start_hour);
-        const end = new Date(slots[slots.length - 1].end_hour);;
-
-        result.push({
-            booking_id: booking.booking_id,
-            room_id: room?.room_id,
-            room_name: room?.name,
-            customer_name: customer?.name,
-            start,
-            end,
-            duration: (end.getHours() - start.getHours())
-        });
-
+    result.push({
+      booking_id: booking.booking_id,
+      room_id: room?.room_id,
+      room_name: room?.name,
+      customer_name: customer?.name,
+      start,
+      end,
+      duration: end.getHours() - start.getHours(),
     });
+  });
 
-    return result;
+  return result;
 }
