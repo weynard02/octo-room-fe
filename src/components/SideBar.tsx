ab195a0f0b39d6dab6854ab79cde86da62f8a34c
weynard02 <samsicoweynard@gmail.com>
2026-03-12 09:20:56 +0700

    fix minor

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,7 +2,6 @@ import { useState, type ReactNode } from "react";
 import { Link, useLocation } from "react-router-dom";
 import {
   LayoutDashboard,
-  Settings,
   ChevronLeft,
   ChevronRight,
   LogIn,
